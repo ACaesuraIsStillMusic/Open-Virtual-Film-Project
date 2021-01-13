37bf2a04dd4232cd0cbe32dd6c2e4db4ae37f3f6
ACaesuraIsStillMusic <dan.corrigan222@gmail.com>
2021-01-13 08:48:58 -0800

    another clipping plane fix

--- OVFP426/Plugins/OVFPPlugin/Resources/BlenderToUnrealPlugin/pyOVFP_BlenderToUnrealBridge.py
+++ OVFP426/Plugins/OVFPPlugin/Resources/BlenderToUnrealPlugin/pyOVFP_BlenderToUnrealBridge.py
@@ -759,9 +759,10 @@ class OVFPB2U_OT_SetDefaultUnits(bpy.types.Operator):
         scene = context.scene
         mytool = scene.my_tool
         bpy.context.scene.unit_settings.system = 'METRIC'
-        bpy.context.scene.unit_settings.scale_length = 1
+        bpy.context.scene.unit_settings.scale_length = 0.01
         bpy.context.scene.unit_settings.length_unit = 'CENTIMETERS'
         bpy.context.space_data.clip_end = 1e+06
+        bpy.context.space_data.clip_start = 1
 
 
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
